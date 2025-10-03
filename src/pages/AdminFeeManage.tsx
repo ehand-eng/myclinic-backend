3f982e386da30f1aa0f4dc5ab8c5a042e897296c
nalinlibt <nalin@libt.co.uk>
2025-10-03 21:08:34 +0100

    Changed admin dashboard booking form

--- src/pages/AdminFeeManage.tsx
+++ src/pages/AdminFeeManage.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
 import SimpleFeeManagement from '@/components/SimpleFeeManagement';
 
 const AdminFeeManage: React.FC = () => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="container mx-auto px-4 py-8 flex-grow">
-        <SimpleFeeManagement />
-      </main>
-      <Footer />
+    <div className="w-full">
+      <SimpleFeeManagement />
     </div>
   );
 };
