0e32837cba0ae6c15a0d34f9538e6abaaf189367
Amila Kudahewa <akudahewa@gmail.com>
2026-04-01 19:33:54 +0530

    fix the env veriable issues

--- server/routes/whatsappRoutes.js
+++ server/routes/whatsappRoutes.js
@@ -9,7 +9,7 @@ router.get('/webhook', (req, res) => {
     const token = req.query['hub.verify_token'];
     const challenge = req.query['hub.challenge'];
 
-    if (mode === 'subscribe' && token === "myclinic_whatsapp_token") {
+    if (mode === 'subscribe' && token === process.env.WHATSAPP_VERIFY_TOKEN) {
         console.log('✅ WhatsApp webhook verified');
         return res.status(200).send(challenge);
     }
