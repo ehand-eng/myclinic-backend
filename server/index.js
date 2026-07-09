b6af6b08cae278102d9b0979e4e525591dace654
Virul Akbo De Silva <waviruldesilva@gmail.com>
2026-07-09 23:02:01 +0530

    finalize the server dockerization

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const { requestLogger } = require('./utils/logger');
-require('dotenv').config();
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 // Import routes
 const doctorRoutes = require('./routes/doctorRoutes');
